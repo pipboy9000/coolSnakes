b7857250125b0339cafea4084ad52fe6bde2d98e
Dan <dan784@gmail.com>
2019-12-05 00:17:11 +0200

    made snakes even cooler

--- script.js
+++ script.js
@@ -1,66 +1,73 @@
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext("2d");
 
+let width = canvas.width;
+let height = canvas.height;
+
 ctx.fillStyle = "black"
 ctx.strokeStyle = "#29ff7e"
+ctx.lineWidth = 4;
 
-ctx.fillRect(0, 0, 800, 800);
+ctx.fillRect(0, 0, width, height);
 
-let x = 400;
-let y = 400;
-let z = 1;
+let x = width / 2;
+let y = height / 2;
 
-let angle = Math.PI; //90 degrees
+let angle = (Math.floor(Math.random() * 9) - 4) * 0.7853981634;
 
-let angleFlux = 0;
-
-let dist = 5;
-
-let state = 0; // 0: line 1:arc
+function mark(x, y) {
+    ctx.strokeStyle = "red";
+    ctx.beginPath()
+    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.stroke();
+}
 
 function drawNextSegment() {
+
     //fade last
     ctx.fillStyle = "#00000005";
     ctx.fillRect(0, 0, 800, 800);
 
-    //line
-    // let dist = Math.floor(Math.random() * 20);
-    let nextX = x + Math.sin(angle) * dist;
-    let nextY = y + Math.cos(angle) * dist;
+    let dist = Math.random() * 100 + 30;
 
-    z += .4;
-    dist += 0.15;
+    let hue = performance.now() / 10 % 360
 
-    let hue = performance.now() / 10 % 255
-
-    ctx.fillStyle = `hsl(${hue},100%,70%`;
     ctx.strokeStyle = `hsl(${hue},100%,70%`;
+
+    //line
+    let nextX = x + Math.cos(angle) * dist;
+    let nextY = y + Math.sin(angle) * dist;
 
     ctx.beginPath();
     ctx.moveTo(x, y);
-    ctx.lineWidth = z;
     ctx.lineTo(nextX, nextY);
     ctx.stroke();
 
-    let nextAngle = angle + (Math.random() * Math.PI - (Math.PI / 2)) * angleFlux;
+    let nextAngle = (angle + Math.ceil(Math.random() * 3) * 0.7853981634) % (Math.PI * 2);
+
+    let cornerRad = Math.random() * 50 + 10;
+
+    let cornerCenterX = nextX + (Math.cos(angle + Math.PI / 2)) * cornerRad;
+    let cornerCenterY = nextY + (Math.sin(angle + Math.PI / 2)) * cornerRad;
+
+    // mark(cornerCenterX, cornerCenterY);
 
     ctx.beginPath();
+    ctx.arc(cornerCenterX, cornerCenterY, cornerRad, angle - Math.PI / 2, nextAngle - Math.PI / 2, false);
+    ctx.stroke();
 
-    console.log(hue);
+    //set to arc endpoint
+    x = cornerCenterX + Math.cos(nextAngle - Math.PI / 2) * cornerRad;
+    y = cornerCenterY + Math.sin(nextAngle - Math.PI / 2) * cornerRad;
 
-    ctx.arc(nextX, nextY, z / 2, 0, Math.PI * 2);
-    ctx.fill();
+    // mark(x, y);
 
-    x = nextX;
-    y = nextY;
     angle = nextAngle;
-    angleFlux = Math.sin(performance.now() / 250);
 
-    if (x > 800 || x < 0 || y > 800 || y < 0) {
-        x = 400;
-        y = 400;
-        z = 1;
-        dist = 5;
+    if (x > width || x < 0 || y > height || y < 0) {
+        x = width / 2;
+        y = height / 2;
+        angle = (Math.floor(Math.random() * 9) - 4) * 0.7853981634;
     }
 }
 
